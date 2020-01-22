ebc4cc769ceb3118a33e710df8797a35176c7078
TMed34 <mouha.chelali@gmail.com>
2020-01-22 16:35:47 +0100

    TD1 Solution

--- imgProcsJava/src/imgProcsJava/ImageProcessing.java
+++ imgProcsJava/src/imgProcsJava/ImageProcessing.java
@@ -1,104 +1,150 @@
-import java.awt.image.BufferedImage;
+package imgProcsJava;
 
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.core.MatOfByte;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.core.Size;
-
-public class ImageProcessing { 
+public class lancher {
 	
-	public static void imshow(Mat image) throws IOException {
-		 //Encoding the image 
-	      MatOfByte matOfByte = new MatOfByte();       
-	      Imgcodecs.imencode(".jpg", image, matOfByte); 
-
-	      //Storing the encoded Mat in a byte array 
-	      byte[] byteArray = matOfByte.toArray(); 
-
-	      //Preparing the Buffered Image 
-	      InputStream in = new ByteArrayInputStream(byteArray); 
-	      BufferedImage bufImage = ImageIO.read(in); 
-
+	public static void imshow(BufferedImage image) throws IOException {
 	      //Instantiate JFrame 
 	      JFrame frame = new JFrame(); 
 
 	      //Set Content to the JFrame 
-	      frame.getContentPane().add(new JLabel(new ImageIcon(bufImage))); 
+	      frame.getContentPane().add(new JLabel(new ImageIcon(image))); 
 	      frame.pack(); 
 	      frame.setVisible(true);
 	 }
 	
-	 public static void main(String args[]) throws Exception  { 
-	     int width = 963;    //width of the image 
-	     int height = 640;   //height of the image 
+	static BufferedImage bresenham(BufferedImage img, int x1, int y1, int x2, int y2, Color c) 
+	{ 
+		int m_new = 2 * (y2 - y1); 
+		int slope_error_new = m_new - (x2 - x1); 
+
+		for (int x = x1, y = y1; x <= x2; x++) 
+		{ 
+			//System.out.print("(" +x + "," + y + ")\n"); 
+			img.setRGB(x, y, c.getRGB());
+			// Add slope to increment angle formed 
+			slope_error_new += m_new; 
+
+			// Slope error reached limit, time to 
+			// increment y and update slope error. 
+			if (slope_error_new >= 0) 
+			{ 
+				y++; 
+				slope_error_new -= 2 * (x2 - x1); 
+			} 
+		} 
+		return img;
+	}
 	
-	     System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	     
-	     
-	     
-         Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-         System.out.println("mat = " + mat.dump());
-         
-       //Instantiating the Imgcodecs class 
-         Imgcodecs imageCodecs = new Imgcodecs();
-         
-         
-         // Reading images
-         Mat img = Imgcodecs.imread("C:/Users/mchelali/Pictures/landscape.png");
-         System.out.println("Image Loaded");
-         
-         Size sizeA = img.size();
-         
-         int rows = img.rows(); //Calculates number of rows
-         int cols = img.cols(); //Calculates number of columns
-         int ch = img.channels(); //Calculates number of channels (Grayscale: 1, RGB: 3, etc.)
-         
-         System.out.println("The size of the image is " + rows + " " + cols + " " +ch);
-         
-         //img.convertTo(img, CvType.CV_8UC1);
-         for (int i=0; i<rows; i++)
-         {
-             for (int j=0; j<cols; j++)
-             {
-            	 // get pixel values 
-            	 double[] pix = img.get(i, j);
-            	 System.out.println(pix[0] + " " + pix[1] + " " +pix[2]);
-            	 
-            	 //set pixel after precess
-            	 img.put(i, j, pix);
-             }
-         }
-         
-         
-         //Creating the empty destination matrix
-         Mat dst = new Mat();
-         //Converting the image to gray sacle and saving it in the dst matrix
-         Imgproc.cvtColor(img, dst, Imgproc.COLOR_RGB2GRAY);
-         
-         // Converting to binary image...
-         Imgproc.threshold(img, dst, 200, 500, Imgproc.THRESH_BINARY);
-         
-         imshow(img);
-         
-         //Writing the image 
-         imageCodecs.imwrite("C:/Users/mchelali/Pictures/landscape-savedwithJAVA.png", img); 
-         System.out.println("Image Saved ............"); 
-	 }
-	 
-	 
-	 
-	 
+	static BufferedImage drawRect(BufferedImage img, int x1, int y1, int x2, int y2, Color c) 
+	{ 
+		int width  = img.getWidth();
+		int height = img.getHeight();
+		
+		if (x1 > height && x2 > height) {
+			throw new java.lang.Error("x1 ou x2 sont superieur a la hauteur de l'image");
+		}
+		
+		if (y1 > width && y2 > width) {
+			throw new java.lang.Error("y1 ou y2 sont superieur a la largeur de l'image");
+		}
+		
+		for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2 ; y++) {
+            	img.setRGB(x, y, c.getRGB());
+            }
+        }
+		
+		return img;
+	}
+	
+	static BufferedImage zeros(int height, int width) {
+		
+		int[] noir = {0,0,0,255};
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB); 
+        //WritableRaster raster = img.getRaster();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height ; y++) {
+            	img.getRaster().setPixel(x, y, noir);
+            }
+        }
+        return img; 
+    }
+	
+	public static void exo1() {
+		/**************************
+		 * 		     Exo 1		  *
+		 **************************/
+		// TODO Auto-generated method stub
+		File path = new File("C:/Users/mchelali/Pictures/landscape.png");
+
+		BufferedImage img = null;
+		
+		try {
+			img = ImageIO.read(path);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		int width  = img.getWidth();
+		int height = img.getHeight();
+		
+		System.out.println("l = " + height + ";c = " + width);
+		System.out.println("type " + img.getType());
+		
+		// Affichage de l'image
+		try {
+			imshow(img);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int y = (width / 2) - (width / 4);
+		int x = (height / 2) + (height / 4);
+		
+		int p = img.getRGB(x,y); // récupération des couleurs RGB du pixel a la position (x, y)
+		
+		int a = (p>>24)&0xff; 
+        int r = (p>>16)&0xff; 
+        int g = (p>>8)&0xff; 
+        int b = p&0xff; 
+		System.out.println("a = " +  a + " r = " +  r + " g = " + g + " b = " + b);
+	}
+	
+	public static void exo2() {
+		/**************************
+		 * 		     Exo 2		  *
+		 **************************/
+		BufferedImage img2 = zeros(128, 128);
+		img2 = bresenham(img2, 10, 10, 100, 10, Color.BLUE); 
+		img2 = bresenham(img2, 10, 10, 100, 100, Color.CYAN);
+		img2 = bresenham(img2, 10, 10, 100, 30, Color.MAGENTA);
+		img2 = drawRect(img2, 50, 50, 80, 100, Color.GREEN);
+		try {
+			imshow(img2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		exo1();
+		exo2();
+		
+	}
+
 }
